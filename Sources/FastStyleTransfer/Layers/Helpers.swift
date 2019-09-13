285bf1148c86ed3d3a60b1c8b8a546801d014cf7
Vova Manannikov <vmanannikov@broadsoft.com>
2019-09-13 09:08:57 +0300

    update to compile with s4tf 0.5-rc0

--- Sources/FastStyleTransfer/Layers/Helpers.swift
+++ Sources/FastStyleTransfer/Layers/Helpers.swift
@@ -2,7 +2,7 @@ import TensorFlow
 
 /// Layer for padding with reflection over mini-batch of images
 /// Expected input layout is BxHxWxC
-public struct ReflectionPad2d<Scalar: TensorFlowFloatingPoint>: Layer {
+public struct ReflectionPad2d<Scalar: TensorFlowFloatingPoint>: ParameterlessLayer {
     @noDerivative public let padding: ((Int, Int), (Int, Int))
 
     public init(padding: ((Int, Int), (Int, Int))) {
@@ -26,7 +26,7 @@ public struct ReflectionPad2d<Scalar: TensorFlowFloatingPoint>: Layer {
 
 
 /// Layer applying relu activation function
-public struct ReLU<Scalar: TensorFlowFloatingPoint>: Layer {
+public struct ReLU<Scalar: TensorFlowFloatingPoint>: ParameterlessLayer {
     @differentiable
     public func callAsFunction(_ input: Tensor<Scalar>) -> Tensor<Scalar> {
         return relu(input)
